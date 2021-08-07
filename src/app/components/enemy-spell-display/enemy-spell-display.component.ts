db89281dd3bd46ba78d4b464441cb764a4cc00a6
IllusiveDot14 <illusivedot14@gmail.com>
2021-08-07 09:23:35 -0700

    High guides done

--- src/app/components/enemy-spell-display/enemy-spell-display.component.ts
+++ src/app/components/enemy-spell-display/enemy-spell-display.component.ts
@@ -11,6 +11,7 @@ import { HttpService } from 'src/app/services/http.service';
 export class EnemySpellDisplayComponent implements OnInit {
   @Input() skills!: EnemySkill[];
   @Input() category: string = "";
+  @Input() caster: string = "";
   constructor(private router: Router, private _enemyService: HttpService) { }
 
   ngOnInit(): void {
@@ -26,18 +27,8 @@ export class EnemySpellDisplayComponent implements OnInit {
   capitalizeFirstLetter(name: string) : string {
     return name.charAt(0).toUpperCase() + name.slice(1);
   }
-  getEnemySpellMP4(spellName : string, caster : string) : string {
-    if(!Array.isArray(caster))
-    {
-      return "https://ia601502.us.archive.org/5/items/twrpg_guidebook/EnemySpells%2F" + encodeURI(this.category) + '%2F' + encodeURI(caster) + '%2F' + encodeURI(spellName) + ".mp4";
-    }
-    let names = Array.from(caster);
-    for(let skill of this.skills) {
-      if(names.includes(skill.caster)) {
-        return "https://ia601502.us.archive.org/5/items/twrpg_guidebook/EnemySpells%2F" + encodeURI(this.category) + '%2F' + encodeURI(skill.caster) + '%2F' + encodeURI(spellName) + ".mp4";
-      }
-    }
-    return "";
+  getEnemySpellMP4(spellName : string) : string {
+    return "https://ia601502.us.archive.org/5/items/twrpg_guidebook/EnemySpells%2F" + encodeURI(this.category) + '%2F' + encodeURI(this.caster) + '%2F' + encodeURI(spellName) + ".mp4";
   }
   openMinionDetails(id: string) : void {
     const url = this.router.serializeUrl(
@@ -67,7 +58,11 @@ export class EnemySpellDisplayComponent implements OnInit {
       case /^Mystic Shell/ig.test(spellName): return false;
       case /^Hard Shell/ig.test(spellName): return false;
       case /^Infected Swipes/ig.test(spellName): return false;
+      case /^Lifesteal/ig.test(spellName): return false;
+      case /^Deathly Shroud/ig.test(spellName): return false;
       case /^Ent Power/ig.test(spellName): return false;
+      case /^Shadow Attack/ig.test(spellName): return false;
+      case /^Reinforcement of Darkness/ig.test(spellName): return false;
       case /^Grasp of the Fiend/ig.test(spellName): return false;
       default: return true;
   }
