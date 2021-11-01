d98ae7bc53f324e98b1ec3fd063bc938a649f28f
IllusiveDot14 <illusivedot14@gmail.com>
2021-10-31 19:59:55 -0700

    quickfix to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   ITEMS_URL: 'https://raw.githubusercontent.com/sfarmani/twrpg-info/master/items.json',
-  ENEMY_URL: 'https://raw.githubusercontent.com/Illusivedot14/twrpg-info/master/bosses.json',
-  ENEMYSKILL_URL: 'https://raw.githubusercontent.com/Illusivedot14/twrpg-info/master/skills-boss.json'
+  ENEMY_URL: 'https://raw.githubusercontent.com/sfarmani/twrpg-info/master/bosses.json',
+  ENEMYSKILL_URL: 'https://raw.githubusercontent.com/sfarmani/twrpg-info/master/skills-boss.json'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   ITEMS_URL: 'https://raw.githubusercontent.com/sfarmani/twrpg-info/master/items.json',
-  ENEMY_URL: 'https://raw.githubusercontent.com/Illusivedot14/twrpg-info/master/bosses.json',
-  ENEMYSKILL_URL: 'https://raw.githubusercontent.com/Illusivedot14/twrpg-info/master/skills-boss.json'
+  ENEMY_URL: 'https://raw.githubusercontent.com/sfarmani/twrpg-info/master/bosses.json',
+  ENEMYSKILL_URL: 'https://raw.githubusercontent.com/sfarmani/twrpg-info/master/skills-boss.json'
 };
 
 /*
